776fb56953e62cccc174735a591e08791ca63d20
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-02-08 16:57:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+   apiKey: "AIzaSyD7IkywCZjyTI9xNVRqof6NvVtpfHkQJWE",
+  authDomain: "teststory-c3f62.firebaseapp.com",
+  databaseURL: "https://teststory-c3f62-default-rtdb.firebaseio.com",
+  projectId: "teststory-c3f62",
+  storageBucket: "teststory-c3f62.appspot.com",
+  messagingSenderId: "460076816949",
+  appId: "1:460076816949:web:18a3ec39b1d530b797af7c"
+};
---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import StackNavigator from "./StackNavigator";
+import Profile from "../screens/Profile";
+import Logout from "../screens/Logout";
+import firebase from "firebase";
+
+import CustomSidebarMenu from "../screens/CustomSidebarMenu";
+
+const Drawer = createDrawerNavigator();
+
+export default class DrawerNavigator extends Component {
+ 
+  render() {
+    let props = this.props;
+    return (
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Home"
+          component={StackNavigator}
+          options={{ unmountOnBlur: true }}
+        />
+        <Drawer.Screen
+          name="Profile"
+          component={Profile}
+          options={{ unmountOnBlur: true }}
+        />
+        <Drawer.Screen
+          name="Logout"
+          component={Logout}
+          options={{ unmountOnBlur: true }}
+        />
+      </Drawer.Navigator>
+    );
+  }
+}
